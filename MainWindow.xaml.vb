4e7102011b1ae4314367e50549d0dc0faa0dbe7c
imsal <imsal@LAPTOP-RB9O1EMN>
2026-06-08 14:21:42 +1000

    Add project files.

---
+++ MainWindow.xaml.vb
@@ -0,0 +1,21 @@
+﻿Class MainWindow
+    Private Sub ListBox_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub Button_Click_3(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
